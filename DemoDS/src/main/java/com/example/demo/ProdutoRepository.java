d50a71fd70304e6da8e894ed33de18a52134e21c
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2025-07-23 15:42:09 -0300

    Delete DemoDS/src/main/java/com/example/demo/ProdutoRepository.java

--- DemoDS/src/main/java/com/example/demo/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
