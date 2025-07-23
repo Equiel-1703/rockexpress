5888c068d79e881eaed24fb6512004c9e6613327
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2025-07-23 15:41:42 -0300

    Delete DemoDS/src/main/java/com/example/demo/PedidoRepository.java

--- DemoDS/src/main/java/com/example/demo/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-//import com.example.demo.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
