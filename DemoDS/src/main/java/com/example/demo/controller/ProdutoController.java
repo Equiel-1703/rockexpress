20ac9428950ec40fa2de5adcf70f378ce7a5b4d0
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2025-07-29 22:46:03 -0300

    added temporary annotation CORS

--- DemoDS/src/main/java/com/example/demo/controller/ProdutoController.java
+++ DemoDS/src/main/java/com/example/demo/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/produtos")
 public class ProdutoController {
     private final ProdutoService produtoService;
